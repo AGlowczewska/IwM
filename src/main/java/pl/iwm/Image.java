9c559b73676e9e124aeaf8bfc71339a79f1c2250
Mateusz Bartos <geekbajt@gmail.com>
2017-04-04 23:27:06 +0200

    Added image filtering

--- src/main/java/pl/iwm/Image.java
+++ src/main/java/pl/iwm/Image.java
@@ -189,12 +189,69 @@ public class Image {
                 null);
 
         BufferedImage dst = GetCopy(FilteredImage);
-        cop.filter(this.FilteredImage, dst);
-        final ImageIcon icon = new ImageIcon(dst);
+        BufferedImage dst2 = GetCopy(FilteredImage);
+
+        dst2 = gammaCorrection(this.FilteredImage, 0.3);
+//        cop.filter(dst, dst2);
+
+
+
+        final ImageIcon icon = new ImageIcon(dst2);
         FilteredImageLabel.setVisible(true);
         FilteredImageLabel.setIcon(icon);
 
     }
+
+    private static BufferedImage gammaCorrection(BufferedImage original, double gamma) {
+
+        int alpha, red, green, blue;
+        int newPixel;
+
+        double gamma_new = 1 / gamma;
+
+        BufferedImage gamma_cor = new BufferedImage(original.getWidth(), original.getHeight(), original.getType());
+
+        for (int i = 0; i < original.getWidth(); i++) {
+            for (int j = 0; j < original.getHeight(); j++) {
+
+                // Get pixels by R, G, B
+                alpha = new Color(original.getRGB(i, j)).getAlpha();
+                red = new Color(original.getRGB(i, j)).getRed();
+                green = new Color(original.getRGB(i, j)).getGreen();
+                blue = new Color(original.getRGB(i, j)).getBlue();
+
+                red = (int) (255 * (Math.pow((double) red / (double) 255, gamma_new)));
+                green = (int) (255 * (Math.pow((double) green / (double) 255, gamma_new)));
+                blue = (int) (255 * (Math.pow((double) blue / (double) 255, gamma_new)));
+
+                // Return back to original format
+                newPixel = colorToRGB(alpha, red, green, blue);
+
+                // Write pixels into image
+                gamma_cor.setRGB(i, j, newPixel);
+
+            }
+
+        }
+
+        return gamma_cor;
+    }
+
+    // Convert R, G, B, Alpha to standard 8 bit
+    private static int colorToRGB(int alpha, int red, int green, int blue) {
+
+        int newPixel = 0;
+        newPixel += alpha;
+        newPixel = newPixel << 8;
+        newPixel += red;
+        newPixel = newPixel << 8;
+        newPixel += green;
+        newPixel = newPixel << 8;
+        newPixel += blue;
+
+        return newPixel;
+    }
+
 
     public void CalculateTopLight() {
         for (int i = 0; i < pictureWH; i++) {
